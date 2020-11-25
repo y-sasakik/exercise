c7564fe5aae369dfeede454f58b8ad72f3d79a4f
y-sasakik <71587373+y-sasakik@users.noreply.github.com>
2020-11-25 10:21:45 +0900

    Add files via upload

--- exercise2/js/script.js
+++ exercise2/js/script.js
@@ -1,10 +1,40 @@
 $(function(){
-$('.nav_toggle').on('click', function () {
-    $('.nav_toggle, .nav, .nav_menu, .main-content, .nav_menu_li, body, html').toggleClass('show');
+
+
+
+
+  $('.nav_toggle').on('click', function () {
+      $('.nav_toggle, .nav, .nav_menu, .main-content, .nav_menu_li ').toggleClass('show');
   });
 
 
 
 
+$(".nav_menu_li_home").mouseover(function() {
 
-})
+  $(".hover").css('visibility','visible');
+
+});
+$(".nav_menu_li_home").mouseout(function() {
+
+  $(".hover").css('visibility','hidden');
+
+});
+
+
+
+$(".page a").mouseover(function() {
+
+  $(".page_hover li").css('visibility','visible');
+
+});
+$(".page a").mouseout(function() {
+
+  $(".page_hover li").css('visibility','hidden');
+
+});
+
+
+
+
+});
