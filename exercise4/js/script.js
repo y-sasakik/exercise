aeacfcd1352be4acecd7c9d25085cd80a79e0cfb
y-sasakik <71587373+y-sasakik@users.noreply.github.com>
2020-11-29 19:28:10 +0900

    Add files via upload

--- exercise4/js/script.js
+++ exercise4/js/script.js
@@ -6,7 +6,7 @@ $(window).scroll(function() {
 var scroll =$(window).scrollTop();
 
 
-if (scroll > 300){ $(".main-content1 p").css('opacity','1');
+if (scroll > 100){ $(".main-content1 p").css('opacity','1');
    }
 })
 
