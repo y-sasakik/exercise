499c370035e1774f0daf9fc6d94a186b4e1bbf22
y-sasakik <71587373+y-sasakik@users.noreply.github.com>
2020-11-29 18:49:19 +0900

    Add files via upload

---
+++ exercise4/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+$(window).scroll(function() {
+
+
+var scroll =$(window).scrollTop();
+
+
+if (scroll > 300){ $(".main-content1 p").css('opacity','1');
+   }
+})
+
+
+
+});
+
+
+//.css('opacity','1');
